3d0ff282dd3b57f6bf158ac77e2da5c4f7985bbb
Mrk0p <sachinwele23@gmail.com>
2023-03-03 13:05:52 +0530

    Added:calculating monthly wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,24 +4,24 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORING_DAYS = 2;
  
 function getWorkingHours(empCheck) {
     switch (empCheck){
         case IS_PART_TIME :
-            console.log("Employee half time.")
             return PART_TIME_HOURS;
         case IS_FULL_TIME :
-            console.log("Employee full time.")
             return FULL_TIME_HOURS;
         default : 
-        console.log("Employee is absent.")
         return 0;
     }
 }
 
 let empHrs = 0;
-empCheck = Math.floor(Math.random()*10) % 3;
-empHrs = getWorkingHours(empCheck);
+for (let day = 0; day < NUM_OF_WORING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = empHrs + getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage :"+empWage);
+console.log("Total hrs :"+empHrs+"Emp Wage:"+empWage);
 
